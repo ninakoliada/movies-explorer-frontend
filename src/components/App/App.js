ca080c09d9015e64ab5dd13d3869ea034146cfdb
pinakoladda <pinakoladda@ya.ru>
2022-04-10 14:53:56 +0300

    routes

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,39 @@
+import {BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Main } from '../Main/Main';
+import { Movies } from '../Movies/Movies';
+import { SavedMovies } from '../SavedMovies/SavedMovies';
+import { Profile } from '../Profile/Profile';
+import { Register } from '../Register/Register';
+import { Login } from '../Login/Login';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-  
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Switch>
+          <Route path="/" exact>
+            <Main />
+          </Route>
+          <Route path="/movies">
+            <Movies />
+          </Route>
+          <Route path="/saved-movies">
+            <SavedMovies />
+          </Route>
+          <Route path="/profile">
+            <Profile />
+          </Route>
+          <Route path="/sign-in">
+            <Register />
+          </Route>
+          <Route path="/sign-up">
+            <Login />
+          </Route>
+        </Switch>
+      </div>
+    </BrowserRouter>
   );
 }
 
